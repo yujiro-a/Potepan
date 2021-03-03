568817039ce83a169d57adca0700601c4d5d8c07
EC2 Default User <ec2-user@ip-172-31-11-215.us-east-2.compute.internal>
2021-03-03 20:07:43 +0000

    訂正分です

---
+++ Potepan/java-script/stop-watch/main.js
@@ -0,0 +1,45 @@
+const timeElement = document.getElementById('time');
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+
+let h = 0;
+let m = 0;
+let s = 0;
+let ms = 0;
+
+let intervalId = null;
+
+start.addEventListener("click", function(){
+  if(intervalId != null) {
+    return;
+  }
+  intervalId = setInterval(function(){
+    ms++;
+    if(ms == 10) {
+      s++;
+      ms = 0;
+      if(s == 60) {
+        m++;
+        s = 0;
+        if(m == 60) {
+          h++;
+        }
+      }
+    }
+    timeElement.innerHTML = h + ":" + m + ":" + s + ":" + ms;
+  },100);
+  start.disabled = true;
+});
+
+stop.addEventListener("click", function(){
+  clearInterval(intervalId);
+  intervalId = null;
+  start.disabled = false;
+});
+
+reset.addEventListener("click", function(e){
+  intervalId = null;
+  h = m = s = ms = 0;
+  timeElement.innerHTML = h + ":" + m + ":" + s + ":" + ms;
+});
